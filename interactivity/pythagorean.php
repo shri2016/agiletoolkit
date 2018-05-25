ae37794cf6f3e1ff624dec171111edeb69f7a5de
Shrinivas <shrini_deshpande@yahoo.co.uk>
2018-05-25 20:11:39 +0530

    Revert "Revert "added pythogorean to interactivity""
    This reverts commit cc57faa0801262dfab1733af03ffb53bd58dc3ab.

--- interactivity/pythagorean.php
+++ interactivity/pythagorean.php
@@ -14,6 +14,16 @@ $col_left->add(['Image', 'pythagorean.png']);
 $form = $col_mid->add('Form');
 $form->addField('a', 'a = ');
 $form->addField('b', 'b = ');
+$form->buttonSave->set('Calculate');
+$form->onSubmit(function($form) use($col_right) {
+    $a = $form->model['a'];
+    $b = $form->model['b'];
+    $c = $a*$a + $b*$b;
+    return $col_right->jsReload(['result'=>$c]);
+    //return 'success';
+});
 
 $col_right->add('Text')->addParagraph('Using calculation c = sqrt(a * a + b * b):');
-$col_right->add(['Label', 'c =', 'detail' => '?']);
+if (isset($_GET['result'])) {
+    $col_right->add(['Label', 'c =', 'detail' => $_GET['result']]);
+}
