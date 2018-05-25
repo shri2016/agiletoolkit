3b569c27ad10ee3f2953ff49a6e6b7fdd6435980
Shrinivas <shrini_deshpande@yahoo.co.uk>
2018-05-25 14:11:40 +0530

    implemented interactivity in multiplication table

--- interactivity/multiplication.php
+++ interactivity/multiplication.php
@@ -8,16 +8,21 @@ $seg = $app->add(['ui'=>'segment']);
 $seg->add(['Text', 'Select multiplier: ']);
 
 for($i = 1; $i < 10; $i++) {
-    $l = $seg->add(['Label', $i, 'blue']);
+    $l = $seg->add([
+        'Label', 
+        $i, 
+        (isset($_GET['b']) && $_GET['b'] == $i) ? 'yellow' : 'blue'
+        ]);
+    $l->link(['b'=>$i]);
 }
+if (isset($_GET['b'])) {
+    $multiplier = $_GET['b'];
+    $data = [];
+    for($i = 1; $i <= 10; $i++) {
+        $data[] = ['a' => $i, 'b' => $multiplier, 'm' => $multiplier * $i];
+    }
 
-$multiplier = 3;
-
-$data = [];
-for($i = 1; $i < 10; $i++) {
-    $data[] = ['a' => $i, 'b' => $multiplier, 'm' => $multiplier * $i];
+    $table = $app->add('Table');
+    $table->setSource($data, ['a', 'b']);
+    $table->addColumn('m', null, ['caption'=>'A * B']);
 }
-
-$table = $app->add('Table');
-$table->setSource($data, ['a', 'b']);
-$table->addColumn('m', null, ['caption'=>'A * B']);
