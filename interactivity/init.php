d674d8fab9c305e11ba898914f6506624a107b84
Shrinivas <shrini_deshpande@yahoo.co.uk>
2018-05-25 20:10:42 +0530

    Revert "add interactivity to the menu"
    This reverts commit fce0f6c63d40b244cbf57ec1d62c356933ba5aba.

--- interactivity/init.php
+++ interactivity/init.php
@@ -6,6 +6,6 @@ $app = new \atk4\ui\App('Interactivity');
 $app->initLayout('Centered');
 
 $menu = $app->add('Menu');
-$menu->addItem(['Multiplication Table', 'icon'=>'table'], ['multiplication']);
-$menu->addItem(['Pythagorean theorem', 'icon'=>'superscript'], ['pythagorean']);
-$menu->addItem(['Guess the number', 'icon'=>'game'], ['guess1']);
+$menu->addItem(['Multiplication Table', 'icon'=>'table']);
+$menu->addItem(['Pythagorean theorem', 'icon'=>'superscript']);
+$menu->addItem(['Guess the number', 'icon'=>'game']);
